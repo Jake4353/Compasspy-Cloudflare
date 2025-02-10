4e410d1aafaf0d23071f26f799d7d68e6fb4594f
Jacob Kowalski <44424544+Jake4353@users.noreply.github.com>
2025-02-10 13:04:52 +1100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='compasspy',
     version='4.0',
-    description='Fork of Artucuno's API client for compass.education',
+    description='Fork of Artucunos API client for compass.education',
     url='https://github.com/Jake4353/Compasspy-Cloudflare',
     author='Jacob Kowalski and Artucuno',
     author_email='artucunov@gmail.com',
